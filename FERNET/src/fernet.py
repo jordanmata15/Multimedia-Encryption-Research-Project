66528fef7a925580330d5c29fc97925e30635673
Jordan Mata <jordanmata15@gmail.com>
2022-11-30 12:05:42 -0800

    Fixed Fernet logic. Working now.

--- FERNET/src/fernet.py
+++ FERNET/src/fernet.py
@@ -1,41 +1,46 @@
+#!/usr/bin/python3
+
 #prerequisite
 #%pip install cryptography
 
+import sys
 from cryptography.fernet import Fernet
-key = Fernet.generate_key()
- 
-# string the key in a file
-with open('filekey.key', 'wb') as filekey:
-   filekey.write(key)
 
-with open('filekey.key', 'rb') as filekey:
-    key = filekey.read()
- 
-# using the generated key
-fernet = Fernet(key)
- 
-# opening the original file to encrypt
-with open('data/data_to_encrypt/alice29.txt', 'rb') as file:
-    original = file.read()
-     
-# encrypting the file
-encrypted = fernet.encrypt(original)
- 
-# opening the file in write mode and
-# writing the encrypted data
-with open('data/encrypted_data/test.txt', 'wb') as encrypted_file:
-    encrypted_file.write(encrypted)
 
-fernet = Fernet(key)
- 
-# opening the encrypted file
-with open('data/encrypted_data/test.txt', 'rb') as enc_file:
-    encrypted = enc_file.read()
- 
-# decrypting the file
-decrypted = fernet.decrypt(encrypted)
- 
-# opening the file in write mode and
-# writing the decrypted data
-with open('data/encrypted_data/detest.txt', 'wb') as dec_file:
-    dec_file.write(decrypted)
+def encrypt(input_file, output_file):
+    key = open("secret.key", "rb").read()
+    fernet = Fernet(key)
+
+    with open(input_file, 'rb') as file:
+        original = file.read()
+
+    encrypted = fernet.encrypt(original)
+
+    with open(output_file, 'wb') as encrypted_file:
+        encrypted_file.write(encrypted)
+
+
+
+def decrypt(input_file, output_file):
+    key = open("secret.key", "rb").read()
+    fernet = Fernet(key)
+
+    with open(input_file, 'rb') as enc_file:
+        encrypted = enc_file.read()
+    decrypted = fernet.decrypt(encrypted)
+
+    with open(output_file, 'wb') as dec_file:
+        dec_file.write(decrypted)
+
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 4:
+        raise Exception("Unexpected number of args!")
+
+    input_filename = sys.argv[2]
+    output_filename = sys.argv[3]
+    if (sys.argv[1] == 'encrypt'):
+        encrypt(input_filename, output_filename)
+    else:
+        decrypt(input_filename, output_filename)
