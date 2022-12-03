e107bb8f2d3bef593a7c92b68ff40468b7829613
Hart Russell <hortisimo@hortisimo>
2022-12-03 01:59:14 -0800

    added new algo and uncompressed versions of each, plus fixed overwrite bug

--- CAMELLIA128/run.sh
+++ CAMELLIA128/run.sh
@@ -4,4 +4,4 @@ enc_input_file=$1
 enc_output_file=$2
 
 # This calls the the algorithm with the specified file as input, the specified output file
-gpg --symmetric --batch --yes --passphrase 'ComputerScience' --cipher-algo 3DES -o "$enc_output_file" "$enc_input_file"
+gpg --symmetric --batch --yes --passphrase 'ComputerScience' --cipher-algo CAMELLIA128 -o "$enc_output_file" "$enc_input_file"
